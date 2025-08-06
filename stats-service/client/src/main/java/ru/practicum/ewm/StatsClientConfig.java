541b855caf793c2fe44602acc816c801c8953351
Oksana Chufistova <oksana.chufistova@gmail.com>
2025-08-06 20:43:28 +0200

    move the statistics server address setting to the client module

---
+++ stats-service/client/src/main/java/ru/practicum/ewm/StatsClientConfig.java
@@ -0,0 +1,9 @@
+package ru.practicum.ewm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class StatsClientConfig {
+}
